4547106927183c12c1d73be78befbe70feae3b50
BCAPATHSHALA <MANOJKUMARGFG.1@GMAIL.COM>
2025-08-08 14:28:01 +0530

    add token schema

---
+++ backend/prisma/migrations/20250808085700_token/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "public"."TokenType" AS ENUM ('ACCESS', 'REFRESH');
+
+-- CreateTable
+CREATE TABLE "public"."Token" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "type" "public"."TokenType" NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token_token_key" ON "public"."Token"("token");
+
+-- AddForeignKey
+ALTER TABLE "public"."Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
