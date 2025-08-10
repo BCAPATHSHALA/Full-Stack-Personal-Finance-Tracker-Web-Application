e2958a106ee5cd5b338e5f599a4c051306165904
BCAPATHSHALA <MANOJKUMARGFG.1@GMAIL.COM>
2025-08-10 21:08:32 +0530

    connect the frontend with backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // Import routes
 import authRoutes from "./routes/auth.routes";
